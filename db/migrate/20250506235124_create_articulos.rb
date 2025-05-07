7bc78a73e81c481827e9e0667fe637138804bd69
unknown <203114550+4lerce@users.noreply.github.com>
2025-05-06 20:38:04 -0400

    Primer sitio

---
+++ db/migrate/20250506235124_create_articulos.rb
@@ -0,0 +1,10 @@
+class CreateArticulos < ActiveRecord::Migration[8.0]
+  def change
+    create_table :articulos do |t|
+      t.string :titulo
+      t.text :contenido
+
+      t.timestamps
+    end
+  end
+end
